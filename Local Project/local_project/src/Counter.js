e0cf5aa8e9d495ae07e4b4adbcdb80f05203242b
mlee788 <mlee788@gatech.edu>
2023-11-02 20:48:53 +0800

    updated UI

--- Local Project/local_project/src/Counter.js
+++ Local Project/local_project/src/Counter.js
@@ -1,27 +1,75 @@
 import React, { useState, useEffect} from "react";
 import './Counter.css';
-import $ from "jquery"
-const Counter = () => {
-  const [counter1, setIndicatorCount] = useState(0);
+import $ from "jquery";
 
+
+const Counter = () => {
+  // useEffect(() => {
+  //   // Fetch indicatorCount from your Flask server
+  //   fetch('/get-indicator-count')
+  //     .then(response => response.json())
+  //     .then(data => setIndicatorCount(data.counter1))
+  //     .catch(error => console.error('Error fetching indicator count:', error));
+  // }, []);
+  const [counter1, setCounter1] = useState(0);
   useEffect(() => {
-    // Fetch indicatorCount from your Flask server
-    fetch('/get-indicator-count')
-      .then(response => response.json())
-      .then(data => setIndicatorCount(data.counter1))
-      .catch(error => console.error('Error fetching indicator count:', error));
+    // Fetch data from the ESP32 server periodically
+    const fetchData = async () => {
+      try {
+        const response = await fetch('172.20.10.2:8000/data'); // Replace with your ESP32 server's URL
+        console.log(response.ok)
+        if (response.ok) {
+          const data = await response.json();
+          console.log('fuck you', data);
+          // Update the state with the received data
+          setCounter1(data.counter1);
+
+        } else {
+          // Handle error
+        }
+      } catch (error) {
+        // Handle network or other errors
+      }
+    };
+    console.log(counter1)
+    // Fetch data every 5 seconds (adjust the interval as needed)
+    const intervalId = setInterval(fetchData, 5000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(intervalId);
   }, []);
+
+  // const utf8EncodeText = new TextEncoder();
+
+  // const str = 'GET_ALL';
+
+  // const byteArray = utf8EncodeText.encode(str);
+
+  // var net = require('net');
+
+  // var client = new net.Socket();
+  // client.connect(8000, '172.20.10.2', function() {
+  //     console.log('Connected');
+  //     client.write(byteArray);
+  // });
+
+  // client.on('data', function(data) {
+  //     console.log('Received: ' + data);
+  //     console.log(data)
+  //     client.destroy(); // kill client after server's response
+  // });
+
   $('documnet').ready(function() {
     var counter1 = 0;
     var counter2 = 0;
     var counter3 = 0;
-    if (counter1 == 0) {
+    if (counter1 === 0) {
       $('.indicator').css('background-color', 'lightgreen');
     }
-    if (counter2 == 0) {
+    if (counter2 === 0) {
       $('.indicator1').css('background-color', 'lightgreen');
     }
-    if (counter3 == 0) {
+    if (counter3 === 0) {
       $('.indicator2').css('background-color', 'lightgreen');
     }
     if (counter1 > 0) {
--- Local Project/local_project/src/pressure.py
+++ Local Project/local_project/src/pressure.py
@@ -1,10 +1,28 @@
 from flask import Flask, request, jsonify
+import socket
 
+PORT = 8000
 app = Flask(__name)
 counter = 0
 
+
+
+def getAll():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('172.20.10.2', PORT))
+    sock.send(b"GET_ALL")
+
+    response = sock.recv(4096)
+    sock.close()
+    print(response)
+    print(response.decode())
+    
+    return response.decode()
+
 @app.route('/update-indicator', methods=['POST'])
 def update_indicator():
+    indicator = getAll()
+    print(indicator)
     global indicatorCount
     data = request.get_json()
     
@@ -23,4 +41,4 @@ def get_indicator_count():
     return jsonify({"indicatorCount": indicatorCount})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='172.20.10.2', port=8000)
