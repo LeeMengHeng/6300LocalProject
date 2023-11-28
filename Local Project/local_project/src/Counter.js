7086e2e3ee1f7c58257b8cee3e2cf2288b9d41e2
mlee788 <mlee788@gatech.edu>
2023-11-29 00:09:17 +0800

    added exclamation mark

--- Local Project/local_project/src/Counter.js
+++ Local Project/local_project/src/Counter.js
@@ -37,7 +37,7 @@ const Counter = () => {
 
     // Clean up the interval on component unmount
     return () => clearInterval(intervalId);
-  }, []);
+}, []);
 
   // const utf8EncodeText = new TextEncoder();
 
@@ -59,41 +59,69 @@ const Counter = () => {
   //     client.destroy(); // kill client after server's response
   // });
 
-  $('documnet').ready(function() {
-    var counter1 = 0;
-    var counter2 = 0;
-    var counter3 = 0;
-    var light = 1;
-    if (counter1 === 0) {
-      $('.indicator').css('background-color', 'lightgreen');
-    }
-    if (counter2 === 0) {
-      $('.indicator1').css('background-color', 'lightgreen');
-    }
-    if (counter3 === 0) {
-      $('.indicator2').css('background-color', 'lightgreen');
-    }
-    if (counter1 > 0) {
-      $('.indicator').css('background-color', 'red');
-    }
-    if (counter2 > 0) {
-      $('.indicator1').css('background-color', 'red');
-    }
-    if (counter3 > 0) {
-      $('.indicator2').css('background-color', 'red');
-    }
-    if (light == 0){
-      $('.light').css('background', '#f9f981');
-      $('.after').css('border-style', '')
-      $('.after').css('border-color', '')
-      $('.after').css('background', '#f9f981')
-    }
-    if (light > 0){
-      $('.after').css('border-style', 'solid')
-      $('.after').css('border-color', 'black')
-      $('.after').css('background', 'white')
-      $('.light').css('background', 'black');
-    }
+var stopBlinking = false;
+setTimeout(function() 
+{
+  stopBlinking = true;
+}, 8000);
+
+function blink(selector) {
+    $(selector).fadeOut('slow', function() {
+        $(this).fadeIn('slow', function() {
+            if (!stopBlinking)
+            {
+                blink(this);
+            }
+            else
+            {
+                $(this).hide();
+            }
+        });
+    });
+}
+blink(".exc");
+
+$('documnet').ready(function() {
+  var counter1 = 0;
+  var counter2 = 0;
+  var counter3 = 0;
+  var light = 1;
+  if (counter1 === 0) {
+    $('.indicator').css('background-color', 'lightgreen');
+  }
+  if (counter2 === 0) {
+    $('.indicator1').css('background-color', 'lightgreen');
+  }
+  if (counter3 === 0) {
+    $('.indicator2').css('background-color', 'lightgreen');
+  }
+  if (counter1 > 0) {
+    $('.indicator').css('background-color', 'red');
+  }
+  if (counter2 > 0) {
+    $('.indicator1').css('background-color', 'red');
+  }
+  if (counter3 > 0) {
+    $('.indicator2').css('background-color', 'red');
+  }
+  if (light == 0){
+    $('.exc').css('color', 'white');
+  }
+  if (light == 1){
+    $('.exc').css('color', 'red');
+  }
+  // if (light == 0){
+  //   $('.light').css('background', '#f9f981');
+  //   $('.after').css('border-style', '')
+  //   $('.after').css('border-color', '')
+  //   $('.after').css('background', '#f9f981')
+  // }
+  // if (light > 0){
+  //   $('.after').css('border-style', 'solid')
+  //   $('.after').css('border-color', 'black')
+  //   $('.after').css('background', 'white')
+  //   $('.light').css('background', 'black');
+  // }
 });
   return (
 <div id="plan">
@@ -110,6 +138,7 @@ const Counter = () => {
       <div class="base"></div>
       <div class="light"></div>
       <div class="after"></div>
+      <div class="exc">!</div>
     </div>
     <div class="door"></div>
   </div>
